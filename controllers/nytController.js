170221da82d45eb33ecb91a7d9b779a2c01ce448
Neil Mosunic <neilm813@gmail.com>
2017-01-12 22:50:07 -0800

    added microsoft sentiment api

--- controllers/nytController.js
+++ controllers/nytController.js
@@ -1,6 +1,55 @@
+require('dotenv').load();
 var axios = require('axios');
 var sentiment = require('../controllers/sentiment');
-require('dotenv').load();
+
+var sentObj =
+	{
+		"documents": [
+			{
+				"language": "en",
+				"id": "1",
+				"text": ""
+			}
+		]
+	};
+
+function getArticles(year, month) {
+  (function callNYT(req, res) {
+    var key = process.env.NYT_KEY;
+    console.log(`nyt key: ${key}`);
+
+    axios.get(`https://api.nytimes.com/svc/archive/v1/${year}/${month}.json?api-key=${key}`)
+      .then(function (response) {
+
+        sentObj.documents[0].text = returnSentimentText(response.data.response.docs);
+
+        sentiment.msSentiment(sentObj);
+
+        // Call to old sentiment API
+        // sentiment.getSentiment(returnSentimentText(response.data.response.docs));
+      })
+      .catch(function (error) {
+        console.log(error);
+        console.log('ERROR!');
+      })
+  })();
+}
+// getArticles('2017', '1');
+
+function returnSentimentText(articles) {
+  var sentimentText = '';
+
+  for (var i = 0; i < articles.length; i++) {
+    sentimentText += (articles[i].headline.main + '. ');
+  }
+  return sentimentText;
+}
+
+
+module.exports = {
+  getArticles: getArticles
+}
+
 
 /*function index(req, res) {
   var key = process.env.NYT_KEY;
@@ -17,37 +66,3 @@ require('dotenv').load();
       // res.json(error);
     })
 }*/
-
-function getArticles(year, month) {
-  (function callNYT(req, res) {
-    var key = process.env.NYT_KEY;
-    console.log(`nyt key: ${key}`);
-
-    axios.get(`https://api.nytimes.com/svc/archive/v1/${year}/${month}.json?api-key=${key}`)
-      .then(function (response) {
-        // res.json(response.data.response.docs);
-        console.log(response.data.response.docs);
-        sentiment.getSentiment(returnSentimentText(response.data.response.docs));
-      })
-      .catch(function (error) {
-        console.log(error);
-        console.log('ERROR!');
-        // res.json(error);
-      })
-  })();
-}
-
-function returnSentimentText(articles) {
-  var sentimentText = '';
-
-  for (var i = 0; i < articles.length; i++) {
-    sentimentText += (articles[i].headline.main + '. ');
-  }
-  return sentimentText;
-}
-
-getArticles('2017', '1');
-
-module.exports = {
-  getArticles: getArticles
-}
--- controllers/sentiment.js
+++ controllers/sentiment.js
@@ -1,19 +1,55 @@
+require('dotenv').load();
 var unirest = require('unirest');
+var axios = require('axios');
 
 var text = text;
 
 var getSentiment = (text) => {
-    unirest.post("https://twinword-sentiment-analysis.p.mashape.com/analyze/")
-        .header("X-Mashape-Key", "XWwI1lRtyEmsh1g9SDGRVs5BlBChp1LZoHCjsnjuhLqJTLgvws")
-        .header("Content-Type", "application/x-www-form-urlencoded")
-        .header("Accept", "application/json")
-        .send(`text=${text}`)
-        .end((result) => {
-            console.log(result.body);
+	unirest.post("https://twinword-sentiment-analysis.p.mashape.com/analyze/")
+		.header("X-Mashape-Key", "XWwI1lRtyEmsh1g9SDGRVs5BlBChp1LZoHCjsnjuhLqJTLgvws")
+		.header("Content-Type", "application/x-www-form-urlencoded")
+		.header("Accept", "application/json")
+		.send(`text=${text}`)
+		.end((result) => {
+			console.log(result.body);
 
-        });
+		});
 }
 
+var msSentiment = (obj, req, res) => {
+	var apikey = process.env.SENTIMENT;
+	console.log(apikey);
+	var config = {
+		headers: {
+			'Ocp-Apim-Subscription-Key': apikey,
+			'Content-Type': 'application/json',
+			'Accept': 'application/json'
+		}
+	}
+	axios.post('https://westus.api.cognitive.microsoft.com/text/analytics/v2.0/sentiment', obj, config)
+		.then(function (response) {
+			console.log(response.data);
+		})
+		.catch(function (error) {
+			console.log(error);
+		})
+}
+
+/*msSentiment(
+	{
+		"documents": [
+			{
+			"language": "en",
+			"id": "1",
+			"text": "This is effin awesome!"
+			}
+		]
+	}
+)*/
+
+// MS Sentiment POST object template
+
 module.exports = {
-  getSentiment
+	getSentiment,
+	msSentiment
 }
